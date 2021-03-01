e9d86c09a8a71b0536c70370910bc3bf3158af26
osidekyle <hawkinskyle67@gmail.com>
2021-03-01 08:30:30 -0800

    1/2

---
+++ currency-converter/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms"
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ConverterComponent } from './converter/converter.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ConverterComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ currency-converter/src/app/converter/converter.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.css']
+})
+export class ConverterComponent implements OnInit {
+  amount=1;
+  from ="CAD"
+  to="USD"
+  rate=0.74
+
+  convert():number{
+    return this.amount*this.rate
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
